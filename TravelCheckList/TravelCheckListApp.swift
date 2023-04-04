a5269777dac50f6a9daf846f840815a367a0ac2e
yigitkarakurt <yigitkarakurt35@gmail.com>
2023-04-04 13:57:26 +0300

    Initial Commit

---
+++ TravelCheckList/TravelCheckListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCheckListApp.swift
+//  TravelCheckList
+//
+//  Created by Yigit Karakurt on 4.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelCheckListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
